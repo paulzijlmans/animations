719c6eebc6f966520e130ccad1a6032c018fba4c
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-05-25 21:49:57 +0200

    added animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,92 @@
-import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  animations: [
+    trigger("divState", [
+      state(
+        "normal",
+        style({
+          "background-color": "red",
+          transform: "translateX(0)",
+        })
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px)",
+        })
+      ),
+      transition("normal <=> highlighted", animate(300)),
+      // transition("highlighted => normal", animate(800)),
+    ]),
+    trigger("wildState", [
+      state(
+        "normal",
+        style({
+          "background-color": "red",
+          transform: "translateX(0) scale(1)",
+        })
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px) scale(1)",
+        })
+      ),
+      state(
+        "shrunken",
+        style({
+          backgroundColor: "green",
+          transform: "translateX(0px) scale(0.5)",
+        })
+      ),
+      transition("normal => highlighted", animate(300)),
+      transition("highlighted => normal", animate(800)),
+      transition("shrunken <=> *", [
+        style({
+          "background-color": "orange",
+        }),
+        animate(
+          1000,
+          style({
+            borderRadius: "50px",
+          })
+        ),
+        animate(500),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  list = ['Milk', 'Sugar', 'Bread'];
+  state = "normal";
+  wildState = "normal";
+  list = ["Milk", "Sugar", "Bread"];
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAnimate() {
+    this.state == "normal"
+      ? (this.state = "highlighted")
+      : (this.state = "normal");
+    this.wildState == "normal"
+      ? (this.wildState = "highlighted")
+      : (this.wildState = "normal");
+  }
+
+  onShrink() {
+    this.wildState = "shrunken";
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
 }
